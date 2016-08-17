756ff032b477055658cc45e0679a0ec14df52064
Jez Humble <jez.humble@gsa.gov>
2016-08-16 18:01:46 -0700

    Billing script now works with marketplace csvs too (no tags)

--- scripts/billing.py
+++ scripts/billing.py
@@ -10,13 +10,14 @@ pp = pprint.PrettyPrinter(indent=4)
 parser = argparse.ArgumentParser(description='Aggregate AWS billing data by account and tag.')
 parser.add_argument('name', help="Provider name")
 parser.add_argument('billing_csv', help="Billing CSV file")
-parser.add_argument('tag_name', nargs='+', help="Names of tags to aggregate by")
+parser.add_argument('tag_name', nargs='*', help="Names of tags to aggregate by")
 args = parser.parse_args()
 
 costs = {}
 totals = {}
 statement = 0
 month = ''
+no_tags = len(args.tag_name) == 0
 
 with open(args.billing_csv) as csvfile:
     reader = csv.DictReader(csvfile)
@@ -29,13 +30,15 @@ with open(args.billing_csv) as csvfile:
             month = match.group()
         elif row['RecordType'] == 'LineItem':
             if row['LinkedAccountId'] not in costs:
-                costs[row['LinkedAccountId']] = {}
+                costs[row['LinkedAccountId']] = { '' : { '' : 0 } } if no_tags else {}
                 for tag in args.tag_name:
                     costs[row['LinkedAccountId']][tag] = {}
             for tag in args.tag_name:
                 if row['user:' + tag] not in costs[row['LinkedAccountId']][tag]:
                     costs[row['LinkedAccountId']][tag][row['user:' + tag]] = 0
                 costs[row['LinkedAccountId']][tag][row['user:' + tag]] = costs[row['LinkedAccountId']][tag][row['user:' + tag]] + float(row['UnBlendedCost'])
+            if no_tags:
+                costs[row['LinkedAccountId']][''][''] = costs[row['LinkedAccountId']][''][''] + float(row['UnBlendedCost'])
 
 output = { 'provider' : args.name, 'month' : month, 'costs' : costs, 'totals' : totals, 'statement' : statement }
 
