49d3ff47cfb728d1543c3bdec86391d058ab59f4
Jez Humble <jez.humble@gsa.gov>
2016-08-26 14:38:05 -0700

    We now provide a helpful message instead of an error when no billing data is loaded

--- public/show_billing.php
+++ public/show_billing.php
@@ -10,13 +10,22 @@ $months = [];
     $month = substr($value->invoice_date, 0, 7);
     $months[$month] = $month;
 });
-$selected_month = key_exists("month", $_REQUEST) ? $_REQUEST["month"] : array_keys($months)[0];
-$billing_data = BillingAdministrator::byMonth($selected_month);
+if (count($months) > 0) {
+    $selected_month = key_exists("month", $_REQUEST) ? $_REQUEST["month"] : array_keys($months)[0];
+    $billing_data = BillingAdministrator::byMonth($selected_month);
+}
 
 include "header.php";
 ?>
 <div class="container-fluid">
     <h1>Billing</h1>
+<?if (count($months) == 0) {?>
+    Chandika has no billing data available.
+    To load billing data into Chandika from Amazon AWS, set up "Detailed Billing Report with Resources and Tags"
+    as described <a href="http://docs.aws.amazon.com/awsaccountbilling/latest/aboutv2/detailed-billing-reports.html">here</a>.
+    Once you have downloaded the csv file, you can import it into Chandika
+    using <a href="https://github.com/18F/chandika/blob/master/scripts/billing.py">this script</a>.
+<?} else {?>
     <form action="show_billing.php" method="get">
         Available months: <?= Filter::dropdown("month", $months, $selected_month) ?>
         <button type="submit" name="action" value="filter">Go</button>
@@ -35,6 +44,7 @@ include "header.php";
         }
         ?>
     </table>
+<?}?>
 </div>
 </body>
 </html>
